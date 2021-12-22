38ddbf07d77708c641ebf0993553e7c62df182a3
Mazooj <mazoozmohammed@gmail.com>
2021-12-22 19:51:28 +0530

    main file updated

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import csv
 from collections import Counter
 
 app = FastAPI()
+file = open('battles.csv', 'r')
+data = csv.reader(file)
+
 
 @app.get('/')
 async def home_page():
@@ -13,10 +16,8 @@ async def home_page():
 @app.get('/battles')
 async def battles_page():
     battles = []
-    with open('battles.csv','r') as file:
-        data = csv.reader(file)
-        for row in data:
-            battles.append(row[0])
+    for row in data:
+        battles.append(row[0])
     return {'Battles': battles[1:]}
 
 
@@ -24,27 +25,22 @@ async def battles_page():
 @app.get('/battle/{id}')
 async def battles_page(id):
     local_dict = {}
-    with open('battles.csv','r') as file:
-        data = csv.reader(file)
-        for rows in data:
-            local_dict.update({rows[2]:rows[0]})
+    for rows in data:
+        local_dict.update({rows[2]:rows[0]})
     return {'Battle':local_dict[id]}
 
 
 # Count of battles per battle type
 @app.get('/count_battles')
 async def battles_page():
-    battle_type_dict = {}
-    with open('battles.csv', 'r') as file:
-        data = csv.reader(file)
 
-        list_of_battles = []
-        battle_type = []
-        for rows in data:
-            list_of_battles.append(rows[0])
-            battle_type.append(rows[14])
-        battle_type.pop(0)
-        count_dict = Counter(battle_type)
+    list_of_battles = []
+    battle_type = []
+    for rows in data:
+        list_of_battles.append(rows[0])
+        battle_type.append(rows[14])
+    battle_type.pop(0)
+    count_dict = Counter(battle_type)
 
     return count_dict
 
@@ -52,17 +48,17 @@ async def battles_page():
 # Most active attacker and defender kings in terms of battles they've fought
 @app.get('/most_active_kings')
 async def battles_page():
-    with open('battles.csv', 'r') as file:
-        data = csv.reader(file)
-        attacker_kings = []
-        defender_kings = []
-        for rows in data:
-            attacker_kings.append(rows[3])
-            defender_kings.append(rows[4])
-        attacker_kings.pop(0)
-        defender_kings.pop(0)
-        count_att = Counter(attacker_kings)
-        count_def = Counter(defender_kings)
+
+    attacker_kings = []
+    defender_kings = []
+    for rows in data:
+        attacker_kings.append(rows[3])
+        defender_kings.append(rows[4])
+
+    attacker_kings.pop(0)
+    defender_kings.pop(0)
+    count_att = Counter(attacker_kings)
+    count_def = Counter(defender_kings)
     return {'attacker kings': count_att,'defender kings':count_def}
 
 
@@ -70,15 +66,15 @@ async def battles_page():
 @app.get('/min_max_avg_defender_size')
 async def battles_page():
     defender_size_list = []
-    with open('battles.csv', 'r') as file:
-        data = csv.reader(file)
-        for rows in data:
-            defender_size_list.append(rows[18])
-        defender_size_list.pop(0)
-        while "" in defender_size_list:
-            defender_size_list.remove("")
-        defender_size_list = list(map(int, defender_size_list))
+
+    for rows in data:
+        defender_size_list.append(rows[18])
+    defender_size_list.pop(0)
+    while "" in defender_size_list:
+        defender_size_list.remove("")
+    defender_size_list = list(map(int, defender_size_list))
 
     return {'Minimum defender size': min(defender_size_list),
             'Maximum defender size': max(defender_size_list),
             'Average defender size': sum(defender_size_list)/len(defender_size_list)}
+
